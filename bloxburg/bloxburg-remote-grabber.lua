62aa1a2cd599795ee8742bd2f8f39c97a0a5f609
vozoid <62110949+vozoid@users.noreply.github.com>
2022-02-15 11:18:38 +0500

    Update bloxburg-remote-grabber.lua

--- bloxburg/bloxburg-remote-grabber.lua
+++ bloxburg/bloxburg-remote-grabber.lua
@@ -1,16 +1,7 @@
 local remotes = {}
 
-setmetatable(remotes, {
-    __newindex = function(t, k, v)
-        rawset(t, k:lower(), v)
-    end,
-    __index = function(t, k)
-        return rawget(t, k:lower())
-    end
-})
-
 for _, v in next, getgc() do
-    if typeof(v) == "function" then
+    if type(v) == "function" then
         if getinfo(v).name == "remoteAdded" then
             local hashes = getupvalue(v, 1)
             for hash, type in next, getupvalue(getupvalue(v, 2), 1) do
