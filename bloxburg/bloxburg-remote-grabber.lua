6b8758649f2f6a63c567088c1eb4b4f15d1b01de
vozoid <62110949+vozoid@users.noreply.github.com>
2022-02-15 12:01:56 +0500

    Update bloxburg-remote-grabber.lua

--- bloxburg/bloxburg-remote-grabber.lua
+++ bloxburg/bloxburg-remote-grabber.lua
@@ -1,19 +1,10 @@
-local remotes = {}
+local fireserver = require(game:GetService("ReplicatedStorage").Modules.DataManager).FireServer
+local get = getupvalue(fireserver, 4)
 
-for _, v in next, getgc() do
-    if type(v) == "function" then
-        if getinfo(v).name == "remoteAdded" then
-            local hashes = getupvalue(v, 1)
-            for hash, type in next, getupvalue(getupvalue(v, 2), 1) do
-                local remote = hashes[hash]
-                remotes[type:gsub("F_", "")] = function(data) 
-                    if remote:IsA("RemoteEvent") then
-                        remote:FireServer(data)
-                    elseif remote:IsA("RemoteFunction") then
-                        remote:InvokeServer(data)
-                    end
-                end
-            end
-        end
-    end
+function fire(type, data)
+    return get(type):FireServer(data)
+end
+
+function invoke(type, data)
+    return get(type, true):InvokeServer(data)
 end
