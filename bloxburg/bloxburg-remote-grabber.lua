cf6054b6db0f6f857af485750d756db6f71bfbc1
vozoid <62110949+vozoid@users.noreply.github.com>
2022-02-15 11:15:57 +0500

    Update bloxburg-remote-grabber.lua

--- bloxburg/bloxburg-remote-grabber.lua
+++ bloxburg/bloxburg-remote-grabber.lua
@@ -10,7 +10,7 @@ setmetatable(remotes, {
 })
 
 for _, v in next, getgc() do
-    if type(v) == "function" then
+    if typeof(v) == "function" then
         if getinfo(v).name == "remoteAdded" then
             local hashes = getupvalue(v, 1)
             for hash, type in next, getupvalue(getupvalue(v, 2), 1) do
