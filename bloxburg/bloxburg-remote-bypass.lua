55e37776c01c0ffd92e94226bb28abc70150ba9e
vozoid <62110949+vozoid@users.noreply.github.com>
2022-02-15 11:13:59 +0500

    Update bloxburg-remote-bypass.lua

--- bloxburg/bloxburg-remote-bypass.lua
+++ bloxburg/bloxburg-remote-bypass.lua
@@ -14,7 +14,13 @@ for _, v in next, getgc() do
         if getinfo(v).name == "remoteAdded" then
             local hashes = getupvalue(v, 1)
             for hash, type in next, getupvalue(getupvalue(v, 2), 1) do
-                remotes[type:gsub("F_", "")] = hashes[hash]
+                remotes[type:gsub("F_", "")] = function(data) 
+                    if hashes[hash]:IsA("RemoteEvent") then
+                        hashes[hash]:FireServer(data)
+                    elseif hashes[hash]:IsA("RemoteFunction") then
+                        hashes[hash]:InvokeServer(data)
+                    end
+                end
             end
         end
     end
